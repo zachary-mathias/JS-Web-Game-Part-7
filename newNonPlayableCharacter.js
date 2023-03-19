c7f0e2886b2c33aa0a0025463395a40086c18798
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-03-19 11:22:18 -0400

    what an absolute day

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,29 +23,39 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    async function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkNorth() {
+    async function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkWest() {
+    async function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkSouth() {
+    async function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+        await sleep(time)
+        stop()
     }
 
-    function stop() {
+    async function stop() {
         direction = null
         element.src = `./assets/red-character/static.gif`
+        await sleep(time)
+        stop()
     }
 
     return {
@@ -57,3 +67,12 @@ function newNonPlayableCharacter(x, y) {
         stop: stop
     }
 }
+
+function sleep(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })  
+}
+
+
+
